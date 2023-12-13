c63cf6fa7e25984206826311641bd7832871c4f8
Luciano Alves <lasbr_alves@outlook.com>
2023-12-13 09:51:39 -0300

    feat: added Board and Piece, also added implementations

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
-import bordgame.Position;
+import bordgame.Board;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Position position = new Position(3, 5);
-		System.out.println(position);
+		Board board = new Board(8, 8);
+		System.out.println(board);
 	}
 }
