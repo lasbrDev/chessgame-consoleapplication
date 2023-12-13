a9476ace1a0438f328352cdfbbaa76ca85d9de36
Luciano Alves <lasbr_alves@outlook.com>
2023-12-13 10:28:23 -0300

    feat: added UI to help build the board also added chess layer

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
 import bordgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
-		System.out.println(board);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 }
