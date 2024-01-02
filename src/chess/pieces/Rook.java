746c3d9c907524c916dc807b92663ed78975629e
Luciano Alves <lasbr_alves@outlook.com>
2024-01-02 15:42:04 -0300

    feat: check logic

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -24,6 +24,7 @@ public class Rook extends ChessPiece {
 		// above
 		p.setValues(position.getRow() - 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
